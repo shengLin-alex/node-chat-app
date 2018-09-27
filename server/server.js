ecd1b2681b13d91affd54f71fb708800edace874
DESKTOP-STF9L29\Sheng-Wei Lin <a182820998@gmail.com>
2018-09-27 14:13:44 +0800

    socket.io connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express =require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('new user.');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnect.');
+    });
+});
+
+server.listen(port, () => {
     console.log('Server is up on port 3000.');
 });
